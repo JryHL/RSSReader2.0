cac985f293e5168dc35298dac720c0ef93457362
JryHL <jerryhl2001@gmail.com>
2024-09-10 23:18:24 -0700

    added cllapse transition

--- frontend/vue-project/src/components/StoryCard.vue
+++ frontend/vue-project/src/components/StoryCard.vue
@@ -6,7 +6,9 @@ import StoryDisplay from '@/components/StoryDisplay.vue';
     <h2>{{ label }}</h2>
     <StoryDisplay v-for="(st, index) in stories.slice(0, 1)" :key="st.id" :story="st" :index="index"/>
     <div class="carousel">
-        <StoryDisplay v-for="(st, index) in showMore ? stories.slice(1) : stories.slice(1,4)" :key="st.id" :story="st" :index="index + 1"/>
+        <TransitionGroup name="more-stories">
+            <StoryDisplay v-for="(st, index) in showMore ? stories.slice(1) : stories.slice(1,4)" :key="st.id" :story="st" :index="index + 1"/>
+        </TransitionGroup>
     </div>
     <button @click="showMore = !showMore"> {{ showMore ? "Collapse" : "Show more"}} </button>
 </template>
@@ -22,8 +24,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 .carousel {
   margin-bottom: 0.5em;
+}
+
+.more-stories-enter-active, .more-stories-leave-active {
+    transition: all 0.2s ease;
+}
+
+.more-stories-leave-to, .more-stories-enter-from {
+    opacity: 0;
+    transform: translateY(-30px);
 }
 </style>
